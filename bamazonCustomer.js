660aeb2fc9d402c9c2654259770eed522b641c12
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-16 11:21:19 -0400

    added functionality the program can now read into mysql database and show all products to the user, also lets the user make purchases and sends that data back to the database to reflect purchases made

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,50 +1,42 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
-
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon"
-});
-
-
-connection.connect(function(err){
-  if(err) throw err;
-  console.log("connected as id " + connection.threadId);
-  //insertItem("pizza", "food", 7.00, 1);
-  //readItems();
-  connection.end();
-});
-
+var connection;
+var newConnection = function(){
+      connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "",
+      database: "bamazon"
+    });
+    connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+  });
+}
+var catalogue = [];
 var insertItem = function(product, department, price, stock_quantity){
   //product, department, price, stock_quantity
   var queryString = 'INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (' + '"' + product + '"' + ', ' + '"' + department + '"' + ', ' + price + ', ' + stock_quantity + ')';
-
   connection.query(queryString, function(err, res){
     if (err) throw err;
 //console.log(res);
   });
 }
-
 /*
 exports.readItems = function(){
   console.log("it works!");
 }
 */
-
-exports.readItems = function(callback){
+exports.readItems = function(){
+    newConnection();
     var queryString = 'SELECT * FROM products';
-
     connection.query(queryString, function(err, res){
       if(err) throw err;
+      catalogue = res;
       console.log("");
       console.log("THE BAMAZON CATALOGUE: ");
       console.log("");
-
       for(i = 0; i < res.length; i++){
         console.log("Item ID: " + res[i].item_id);
         console.log("Product: " + res[i].product_name);
@@ -54,5 +46,61 @@ exports.readItems = function(callback){
         console.log("");
         console.log("");
       }
+      connection.end();
+ // console.log("The cat: " + catalogue);
+      promptUser();
     });
   }
+var placeOrder = function(id, quantity){
+    newConnection();
+  //is there enough of the item?
+  var condition = catalogue[id - 1].stock_quantity - quantity;
+  console.log("condition: " + condition);
+  if(condition < 0) {
+    console.log("Insufficient quantity!");
+  }
+  //There is enough of the item to buy
+  else{
+    var item_id = id - 1;
+    var price = catalogue[item_id].price;
+    var total = price * quantity;
+    var queryString = 'UPDATE products SET stock_quantity = ' + condition + ' WHERE item_id = ' + id;
+    
+    connection.query(queryString, function(err, res){
+      if(err) throw err;
+      connection.end();
+    });
+    console.log("Your total is: $" + total + ".00 Come again!!");
+  }
+}
+//Prompt user to determine what they wish to buy
+var promptUser = function(){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the ID for the product you would like to buy:"
+    },
+    {
+      type: "input",
+      name: "unit",
+      message: "How many units would you like to buy?"
+    }
+  ]).then(function(user){
+    placeOrder(user.id, user.unit);
+  });
+}
+/*
+  var customerPrompt = function(){
+  inquirer.prompt([
+  {
+    type: "list",
+    name: "doThis",
+    message: "What would you like to do?",
+    choices: ["Show Catalogue", "Buy Item"]
+  }
+    ]).then(function(user){
+      
+    });
+}
+*/
--- interface.js
+++ interface.js
@@ -1,28 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var bamazonCustomer = require("./bamazonCustomer.js");
-
-//bamazonCustomer.readItems();
-
 bamazonCustomer.readItems();
-
-
-
-
-var promptUser = function(){
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter the ID for the product you would like to buy:"
-    },
-    {
-      type: "input",
-      name: "unit",
-      message: "How many units would you like to buy?"
-    }
-
-  ]).then(function(user){
-    console.log( "You want: ID: " + user.id + " " + "Unit: " + user.unit );
-  });
-}
+//bamazonCustomer.readItems();
+//testThis(promptUser());
+//promptUser();
